35133f0afd22c4bc963188a98abb7a41be498933
arjun <apkarjundeveloper@gmail.com>
2026-06-30 22:12:42 +0530

    Revert "style: improve formatting of Stack component in CustomActions story"
    This reverts commit 9d8444974231aa62694ebac2cf9d2b723e4c38e6.

--- src/stories/FormWizard.stories.tsx
+++ src/stories/FormWizard.stories.tsx
@@ -236,11 +236,7 @@ export const CustomActions: Story = {
           back,
           submit,
         }: FormWizardActionsParams) => (
-          <Stack
-            direction="row"
-            spacing={2}
-            sx={{ justifyContent: 'space-between', width: '100%' }}
-          >
+          <Stack direction="row" spacing={2} sx={{ justifyContent: 'space-between', width: '100%' }}>
             <Button variant="outlined" onClick={back} disabled={isFirstStep || isSubmitting}>
               ← Back
             </Button>
